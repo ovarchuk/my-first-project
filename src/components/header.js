5fe80c5610f727cd5f2464ec920e390c9e1b91d2
Alexander Varchuk <alexander.varchuk@corezoid.com>
2020-01-05 10:28:42 +0200

    start point

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return <div className='header'>
+    <div>
+      <img src='https://q-cf.bstatic.com/images/hotel/max1024x768/131/131251799.jpg'></img>
+    </div>
+    </div>
+
+}
+
+export default Header;
